ababfad6b44bd9347b6940fdb553754dabbd2654
Saeed Nemati <sn.1361@gmail.com>
2026-08-22 14:56:28 +0330

    committed by script

---
+++ panel/admin/breadcrumb.jsx
@@ -0,0 +1,8 @@
+import PartBreadcrumb from 'partBreadcrumb'
+
+export default props => <PartBreadcrumb
+    icon='precisionManufacturing'
+    name='assets'
+    title='assetsAssets'
+    {...props}
+/>
