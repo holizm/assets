3e9792f99020a78f665a899045191e5d56184c90
Saeed Nemati <sn.1361@gmail.com>
2026-08-23 12:39:24 +0330

    committed by script

--- site/parts/assetSummary.jsx
+++ site/parts/assetSummary.jsx
@@ -1,6 +1,6 @@
 export default ({ asset }) => <dl class='assetSummary'>
-    <dt class='title'>{asset.title}</dt>
-    <dd class='code'>{asset.code}</dd>
-    <dd class='condition'>{asset.assetCondition}</dd>
-    <dd class='status'>{asset.assetStatus}</dd>
+    <dt class='title'>{asset?.title}</dt>
+    <dd class='code'>{asset?.code}</dd>
+    <dd class='condition'>{asset?.assetCondition}</dd>
+    <dd class='status'>{asset?.assetStatus}</dd>
 </dl>
